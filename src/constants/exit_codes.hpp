53ab78a5ab64f7a0ce7d30d3b73edaf45697d3c5
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-17 09:39:19 +0530

    fix: add missing include guards in exit_codes.hpp
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/constants/exit_codes.hpp
+++ src/constants/exit_codes.hpp
@@ -1,9 +1,17 @@
+#ifndef CONSTANTS_EXIT_CODES_HPP
+#define CONSTANTS_EXIT_CODES_HPP
+
 namespace constants {
 
+/**
+ * Exit codes for the program.
+ */
 enum class exit_codes : int {  // NOLINT(performance-enum-size)
   VALID_JSON = 0,
   INVALID_JSON = 1,
   USAGE_ERROR = 2,
 };
 
-}
+}  // namespace constants
+
+#endif  // CONSTANTS_EXIT_CODES_HPP
