850cedb999955570277c221cce77d8f0341c7eab
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-17 11:26:40 +0530

    chore: rename exit_codes to single constants file
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -10,7 +10,13 @@ enum class exit_codes : int {  // NOLINT(performance-enum-size)
   VALID_JSON = 0,
   INVALID_JSON = 1,
   USAGE_ERROR = 2,
+  UNKNOWN_ERROR = 3,
+  FILE_OPERATION_ERROR = 4,
 };
+
+/** How the file is backed: full buffer in memory or memory-mapped. */
+// NOLINTNEXTLINE(performance-enum-size)
+enum class file_load_type { FullMemory, MemoryMapped };
 
 }  // namespace constants
 
